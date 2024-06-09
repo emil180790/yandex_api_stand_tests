63fd0722934529d8ee6675ab9433ac65184cf4d3
Emil <emil180790@mail.ru>
2024-06-09 18:29:21 +0400

    My first commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://cfd806ff-f4eb-4bc7-8743-80a840c99fb6.serverhub.praktikum-services.ru"
+CREATE_USER_PATH = "/api/v1/users/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
---
+++ data.py
@@ -0,0 +1,8 @@
+headers = {
+    "Content-Type" : "application/json"
+}
+user_body = {
+    "firstName": "Аа",
+    "phone": "+74441237887",
+    "address": "г. Москва, ул. Хохотушкина, д. 16"
+}
